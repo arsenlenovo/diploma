8aa5e73dc411b9ee8f1213db0160b9d768dbd5d5
arsenlenovo <arsenarsarsenars85@gmail.com>
2023-04-12 13:58:07 +0600

    added several imports, routes-> route with path and element

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { Route, Routes, } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Contacts from "./pages/Contacts";
+import Home from "./pages/Home";
+import Category from "./pages/Category";
+import NotFound from "./pages/NotFound";
+
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<h1>About</h1>} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/aydin" element={<h1>Aydin</h1>} />
+          <Route path="/categories/:slug" element={<Category />} />
+          
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
+    
     </div>
   );
 }
