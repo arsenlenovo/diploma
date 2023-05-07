9983fabc7405ad100547a0056c7dd0f1de9fdc9d
arsenlenovo <arsenarsarsenars85@gmail.com>
2023-05-07 23:02:54 +0600

    add notification

--- src/components/AddToCart/AddToCart.js
+++ src/components/AddToCart/AddToCart.js
@@ -1,22 +1,33 @@
-import { useContext } from "react";
-import { AppContext } from "../../App";
+import { useContext, useState } from "react";
 import "./AddToCart.css";
+import { AppContext } from "../../App";
 
 export default function AddToCart({ product }) {
   const { cart, setCart } = useContext(AppContext);
+  const [showNotification, setShowNotification] = useState(false);
 
   function onAddToCartClick() {
     const qty = cart[product.id] ? cart[product.id] + 1 : 1;
     setCart({
-      ...cart, // положить текущее содержание корзинки
-      [product.id]: qty, //добавить текущий товар
+      ...cart,
+      [product.id]: qty
     });
+    setShowNotification(true);
+    setTimeout(() => setShowNotification(false), 2000);
   }
 
   return (
     <div className="AddToCart">
-      {cart[product.id] ? cart[product.id] :0}
-      <button onClick={onAddToCartClick}>Add to cart</button>
+      <button onClick={onAddToCartClick}>
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-cart.svg" alt="CART" />
+        Add To Cart
+      </button>
+      {showNotification && (
+        <div className="Notification">
+          <img src="https://cdn-icons-png.flaticon.com/512/1442/1442912.png" alt="CHECKMARK" />
+          <p>Product <mark>successfully</mark> added to cart</p>
+        </div>
+      )}
     </div>
   );
 }
--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,33 +1,65 @@
+// import { useContext } from "react";
+// import { useMatch } from "react-router-dom";
+// import { AppContext } from "../App";
+// import AddToCart from "../components/AddToCart/AddToCart";
+// import NotFound from "./NotFound";
+// import "./Product.css";
+
+// export default function Product() {
+//   const { params } = useMatch("/products/:slug");
+//   const { products } = useContext(AppContext);
+//   const product = products.find((product) => product.slug === params.slug);
+
+//   if (!product) {
+//     return <NotFound />;
+//   }
+
+//   return (
+//     <div className="Product">
+//       <div className="th">
+//         <h1>{product.name}</h1>
+//         <img
+//           className="img-in-product"
+//           src={product.picture}
+//           alt={product.name}
+//         />
+//       </div>
+//       <div className="fd">
+//         <span>{product.price} som</span>
+//         <p className="paragraph-in-product">{product.description}</p>
+//         <p className="paragraph-in-product">{product.description2}</p>
+//         <p className="paragraph-in-product">{product.description3}</p>
+//         <AddToCart />
+//       </div>
+//     </div>
+//   );
+// }
+
 import { useContext } from "react";
-import { useMatch } from "react-router-dom";
+import { useMatch } from "react-router";
 import { AppContext } from "../App";
 import NotFound from "./NotFound";
-import "./Product.css";
+import "./Product.css"
+import AddToCart from "../components/AddToCart/AddToCart";
 
 export default function Product() {
   const { params } = useMatch("/products/:slug");
   const { products } = useContext(AppContext);
+
   const product = products.find((product) => product.slug === params.slug);
 
   if (!product) {
     return <NotFound />;
   }
-
   return (
     <div className="Product">
-      <div className="th">
-        <h1>{product.name}</h1>
-        <img
-          className="img-in-product"
-          src={product.picture}
-          alt={product.name}
-        />
+      <div className="ProductOpen">
+        <h1>{product.description}</h1>
+        <img src={product.picture} alt={product.name} />
       </div>
-      <div className="fd">
-        <span>{product.price} som</span>
-        <p className="paragraph-in-product">{product.description}</p>
-        <p className="paragraph-in-product">{product.description2}</p>
-        <p className="paragraph-in-product">{product.description3}</p>
+      <div>
+        <span>{product.price}₽</span>
+        <AddToCart product={products} />
       </div>
     </div>
   );
