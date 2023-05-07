49003644cc9c9f5327aa085215871d6278c7ba95
arsenlenovo <arsenarsarsenars85@gmail.com>
2023-05-07 18:18:27 +0600

    styling orders and chhange time stop

---
+++ src/components/Auth/Auth.js
@@ -0,0 +1,26 @@
+
+
+import { useContext } from "react";
+import { logIn, logOut } from "../../firebase";
+import { AppContext } from "../../App";
+import { Link } from "react-router-dom";
+
+export default function Auth() {
+  const { user } = useContext(AppContext);
+
+  return (
+    <div className="Auth">
+      {user ? (
+        <span>
+          Hello <Link to="/orders">{user.displayName}</Link>!
+          <button onClick={logOut}>Sign out</button>
+        </span>
+      ) : (
+        <span>
+          Hello guest!
+          <button onClick={logIn}>Sign in</button>
+        </span>
+      )}
+    </div>
+  );
+}
