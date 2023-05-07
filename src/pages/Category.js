9a6ae00eff3d3423548cf9d78b428996bf63ae6d
arsenlenovo <arsenarsarsenars85@gmail.com>
2023-05-07 20:31:01 +0600

    added filtering products

---
+++ src/pages/Category.js
@@ -0,0 +1,27 @@
+import { useContext } from "react";
+import { useMatch } from "react-router-dom";
+import { AppContext } from "../App";
+import ProductsList from "../components/ProductList/ProductList";
+import NotFound from "./NotFound";
+
+export default function Category() {
+  const { params } = useMatch("/categories/:slug");
+
+
+  const { categories } = useContext(AppContext);
+
+  const category = categories.find (
+    (category) => category.slug === params.slug
+  );
+
+  if (!category) {
+    return <NotFound />;
+  }  
+
+  return (
+    <div className="Category">
+      <h1>{category.name}</h1>
+      <ProductsList category={category} />
+    </div>
+  )
+}
