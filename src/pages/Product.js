aaede43c4ae1ca67f806d06b2b25c1e469686d59
arsenlenovo <arsenarsarsenars85@gmail.com>
2023-05-07 12:04:44 +0600

    created a picture, name, price and description for the product

---
+++ src/pages/Product.js
@@ -0,0 +1,33 @@
+import { useContext } from "react";
+import { useMatch } from "react-router-dom";
+import { AppContext } from "../App";
+import NotFound from "./NotFound";
+import "./Product.css";
+
+export default function Product() {
+  const { params } = useMatch("/products/:slug");
+  const { products } = useContext(AppContext);
+  const product = products.find((product) => product.slug === params.slug);
+
+  if (!product) {
+    return <NotFound />;
+  }
+
+  return (
+    <div className="Product">
+      <div className="th">
+        <h1>{product.name}</h1>
+        <img
+          className="img-in-product"
+          src={product.picture}
+          alt={product.name}
+        />
+      </div>
+      <div className="fd">
+        <span>{product.price} som</span>
+        <p className="paragraph-in-product">{product.description}</p>
+        <p className="paragraph-in-product">{product.description2}</p>
+      </div>
+    </div>
+  );
+}
