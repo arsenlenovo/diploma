f6e1ee3e14cc9ec8060afb97405421eba8d1669f
arsenlenovo <arsenarsarsenars85@gmail.com>
2023-05-11 15:19:48 +0600

    add to product list fast loading

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -4,6 +4,7 @@ import "./ProductList.css";
 import { NavLink } from "react-router-dom";
 import AddToCart from "../AddToCart/AddToCart";
 import AddProduct from "../AddProduct/AddProduct";
+import DeleteProduct from "../DeleteProduct/DeleteProduct";
 
 export default function ProductList({ category }) {
   const { products } = useContext(AppContext);
@@ -11,17 +12,19 @@ export default function ProductList({ category }) {
   const output = products
     .filter((product) => product.category === category.id)
     .map((product) => (
-      <div key={product.id} className="Product">
+      <div key={product.id} className="Products">
         <div className="div-inside-product">
           <div className="picture-wrap">
             <img
               className="picture picture--main"
+              loading="eager"
               width="300px"
               src={product.picture}
-              alt={product.name}
+              alt={product.name}   
             />
             <img
               className="picture picture--hover"
+              loading="eager"
               width="300px"
               src={product.picture2}
               alt={product.name}
@@ -33,6 +36,7 @@ export default function ProductList({ category }) {
             </NavLink>
             <span className="price-in-product">{product.price} som</span>
             <AddToCart product={product} />
+            <DeleteProduct product={product} />
           </div>
         </div>
       </div>
@@ -41,7 +45,7 @@ export default function ProductList({ category }) {
   return (
     <div className="ProductList">
       {output}
-      <AddProduct />
+      <AddProduct category={category} />
     </div>
   );
 }
