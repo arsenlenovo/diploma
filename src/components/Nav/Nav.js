6ee9344dd789f24a533694ab53d81279f2cc26e9
arsenlenovo <arsenarsarsenars85@gmail.com>
2023-05-16 15:26:26 +0600

    Change name nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,21 +6,18 @@ export default function Nav() {
     <nav className="Nav">
       <ul className="Ul">
         <li className="NavItem">
-          <NavLink to="/">Men's Watch</NavLink>
+          <NavLink to="/">Home</NavLink>
         </li>
         <li className="NavItem">
-          <NavLink to="/ladies-watch">Ladies Watch</NavLink>
+          <NavLink to="/about">About</NavLink>
         </li>
-        <li className="NavItem">
-          <NavLink to="/wall-clock">Wall Clock</NavLink>
-        </li>
-        <li className="NavItem">
-          <NavLink to="/children's-watch">Children's Watch</NavLink>
-        </li>
-        <li className="NavItem">
-          <NavLink to="/accessories">Accessories</NavLink>
-        </li>
-      </ul>
-    </nav>
+       <li className="NavItem">
+         <NavLink to="/contacts">Contacts</NavLink>
+       </li>
+       <li className="NavItem">
+         <NavLink to="/delivery">Delivery</NavLink>
+       </li>
+     </ul>
+   </nav>
   )
 }
