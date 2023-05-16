7de10481a6785a0fbea655f5d56c1bfcf9dc8b08
arsenlenovo <arsenarsarsenars85@gmail.com>
2023-05-16 23:03:44 +0600

    added new image and span for total then styled them

--- src/components/CartLink/CartLink.js
+++ src/components/CartLink/CartLink.js
@@ -1,3 +1,4 @@
+
 import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { AppContext } from "../../App";
@@ -6,15 +7,14 @@ import "./CartLink.css";
 export default function CartLink() {
   const { cart } = useContext(AppContext);
 
-  const total = Object.values(cart).reduce(
-    (acc, num) => acc + num, 0
-  );
+  const total = Object.values(cart).reduce((acc, num) => acc + num, 0);
 
   return (
     <div className="CartLink">
       <NavLink to="/cart">
-        Cart ({total})
-      </NavLink>  
+        <img className="img-in-cartList" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-cart.svg" alt="cart" />
+        <span className="CartLink-total">{total}</span>
+      </NavLink>
     </div>
-  )
+  );
 }
