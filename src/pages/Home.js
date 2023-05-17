d6775e087a08e6dc95532e0d35a6c3213e8f6c8a
arsenlenovo <arsenarsarsenars85@gmail.com>
2023-05-17 14:43:27 +0600

    Added img in for Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,47 @@
+// import "./Home.css";
+
+// export default function Home() {
+//   return (
+//     <div className="Home">
+//       <h1>Home</h1>
+//       <p>This is </p>
+//        <img src="https://www.watchpro.com/2020/08/bg_new.jpg" alt="backraund" /> 
+           
+//     </div>
+//   )
+// }
+
+import React, { useState } from 'react';
+import './Home.css';
+
 export default function Home() {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const images = [
+    'https://swisstime-exclusive.kg/wp-content/uploads/2020/02/banner-ulysse-nardin.jpg',
+    'https://www.pngplay.com/wp-content/uploads/9/Casio-Transparent-Background.png',
+    'https://wallpapercave.com/wp/wp2009866.jpg',
+    'https://static.tildacdn.com/tild3932-6330-4530-b730-653361616632/casio-gshock-slide.jpeg', // Замените эту ссылку на URL следующего изображения
+    // Добавьте другие ссылки на изображения по мере необходимости
+  ];
+
+  const handleNextImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const handlePreviousImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
     <div className="Home">
       <h1>Home</h1>
-      <p>This is </p>
+      <img className="img-in-Home" src={images[currentImageIndex]} alt="background" />
+      <div>
+      <button onClick={handlePreviousImage}>&#8592;</button> {/* Стрелка влево */}
+        <button onClick={handleNextImage}>&#8594;</button> {/* Стрелка вправо */}
+      </div>
     </div>
-  )
+  );
 }
