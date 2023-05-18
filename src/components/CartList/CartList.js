751fa25139d7fd0321d7ede5dcbf5ae7c2be0b78
arsenlenovo <arsenarsarsenars85@gmail.com>
2023-05-18 14:23:11 +0600

    added input button and added class for link, button and span

--- src/components/CartList/CartList.js
+++ src/components/CartList/CartList.js
@@ -25,15 +25,16 @@ export default function CartList() {
     .map((product) => (
       <div className="CartItem" key={product.id}>
         <img src={product.picture} alt={product.name} />
-        <Link to={"/product/" + product.slug}>{product.name}</Link>
+        <Link className="link" to={"/product/" + product.slug}>{product.name}</Link>
         <input
+          className="input-in-CartList"
           type="number"
           value={cart[product.id]}
           min={1}
           onChange={(event) => onQuantityChange(product, +event.target.value)}
         />
-        <span>{cart[product.id] * product.price} som</span>
-        <button onClick={() => onItemRemove(product)}>Remove</button>
+        <span className="price-in-product">{cart[product.id] * product.price} som</span>
+        <button className="button" onClick={() => onItemRemove(product)}><span className="Remove">Remove</span></button>
       </div>
     ));
 
