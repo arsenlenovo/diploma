130b42053443e3e79fac0c3490daad0373e42067
arsenlenovo <arsenarsarsenars85@gmail.com>
2023-05-18 14:27:31 +0600

    added import for cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,6 +1,6 @@
 import CartList from "../components/CartList/CartList";
 import OrderForm from "../components/OrderForm/OrderForm";
-
+import "./Cart.css";
 export default function Cart() {
   return (
     <div className="Cart">
