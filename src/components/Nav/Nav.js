d102f9318dfc448c68fc41ada38a85f40afca582
arsenlenovo <arsenarsarsenars85@gmail.com>
2023-05-19 15:09:19 +0600

    added logo inside ul and import

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,13 @@
 import { NavLink } from "react-router-dom";
 import "./Nav.css";
 import Search from "../Search/Search";
+import Logo from "../Logo/Logo";
 
 export default function Nav() {
   return (
     <nav className="Nav">
       <ul className="Ul">
+      <Logo />
         <li className="NavItem">
           <NavLink to="/">Home</NavLink>
         </li>
