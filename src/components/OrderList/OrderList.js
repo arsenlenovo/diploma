7a3a0938890d6cac59c3cf51ba1e473fa50b02f9
arsenlenovo <arsenarsarsenars85@gmail.com>
2023-05-19 22:56:29 +0600

    subordinated OrderList deleted all codes and wrote a new code

--- src/components/OrderList/OrderList.js
+++ src/components/OrderList/OrderList.js
@@ -3,13 +3,16 @@ import { AppContext } from "../../App";
 import "./OrderList.css";
 
 export default function OrderList() {
+  // Получить заказы и продукты из контекста
   const { orders, products } = useContext(AppContext);
 
   if (!orders.length || !products.length) {
     return "No orders found.";
   }
-  //выводим все заказы
+
+  // Выводим все заказы.
   const output = orders.map(order => {
+    // Вывести содержимое корзины для этого заказа
     const cartOutput = Object.keys(order.cart).map(productId => {
       const product = products.find(product => product.id === productId);
 
@@ -18,18 +21,19 @@ export default function OrderList() {
       }
 
       return (
-        <div>
+        <div key={product.id}>
+          <img src={product.picture} alt={product.name} />
           {product.name}: {order.cart[productId]} X {product.price} som = {order.cart[productId] * product.price} som
         </div>
       );
     })
 
     return (
-      <div className="Order">
-        <div>Name: {order.name}</div>
-        <div>Phone: {order.phone}</div>
-        <div>Address: {order.address}</div>
-        <div>Cart: {cartOutput}</div>
+      <div key={order.id} className="Order">
+        <div><strong>Name</strong>: {order.name}</div>
+        <div><strong>Phone</strong>: {order.phone}</div>
+        <div><strong>Address</strong>: {order.address}</div>
+        <div><strong>Cart</strong>: {cartOutput}</div>
       </div>
     );
   })
