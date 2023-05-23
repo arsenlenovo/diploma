444054b33e9e41a927f82a029ca4fd86e92a14f2
arsenlenovo <arsenarsarsenars85@gmail.com>
2023-05-23 14:20:00 +0600

    gave class to the paragraph and import About.css

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,24 +1,25 @@
+import "./About.css";
 export default function About() {
   return (
     <div className="About">
       <h1>About</h1>
-      <p>Don't miss a second! We present you a revolutionary watch that will not only show you the exact time, but will also become your stylish accessory!</p>
+      <p className="paragraph-about">Don't miss a second! We present you a revolutionary watch that will not only show you the exact time, but will also become your stylish accessory!</p>
 
-      <p>Our watch collection is inspired by innovative design and cutting-edge technology. Each model is created with love for details to combine elegance, functionality and excellent quality.</p>
+      <p className="paragraph-about">Our watch collection is inspired by innovative design and cutting-edge technology. Each model is created with love for details to combine elegance, functionality and excellent quality.</p>
 
 
-     <p> With our watches you will always be aware of the time and ready for any challenge of life. Stylish cases, comfortable straps and intuitive functions will make using the watch a real pleasure.</p>
+      <p className="paragraph-about"> With our watches you will always be aware of the time and ready for any challenge of life. Stylish cases, comfortable straps and intuitive functions will make using the watch a real pleasure.</p>
 
-    <p>Choose from a variety of styles, from classic and elegant to sporty and modern. We have the perfect watch for every style and occasion.</p>
+      <p className="paragraph-about">Choose from a variety of styles, from classic and elegant to sporty and modern. We have the perfect watch for every style and occasion.</p>
 
-   <p>The watch will also be a great gift for your family and friends. Give them style and functionality that will always remind you of your care.</p>
+      <p className="paragraph-about">The watch will also be a great gift for your family and friends. Give them style and functionality that will always remind you of your care.</p>
 
-   <p>Don't look at time, create it! Refresh your look with our watch collection today and experience the difference.</p>
-   <br/>
-   <p>Time does not wait, so do not miss the chance to own the best watch. Visit our store now and discover the world of style, quality and precision!</p>
-  <br/>
+      <p className="paragraph-about">Don't look at time, create it! Refresh your look with our watch collection today and experience the difference.</p>
+      <br/>
+      <p className="paragraph-about">Time does not wait, so do not miss the chance to own the best watch. Visit our store now and discover the world of style, quality and precision!</p>
+      <br/>
 
-   <h1>Your watch - your style, your time!</h1>
+      <h1 className="h1-about">Your watch - your style, your time!</h1>
     </div>
   )
 }
