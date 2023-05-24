80dfb7886ba2525b45b9bd35d0201e16eda1feea
arsenlenovo <arsenarsarsenars85@gmail.com>
2023-05-24 23:13:55 +0600

    added text for two paragraphs

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -10,6 +10,7 @@ export default function Contacts() {
     <div className="Contacts">
       <div className="contact-details">
       <h1>Contacts</h1>
+      <p className="contacts-paragraph-two">If you have questions, suggestions or want to leave a review, we are always happy to help. We are always ready to listen to your questions, accept your suggestions and take into account your feedback. Contact us right now and we will be happy to help you! Please choose your preferred contact method from the options below:</p>
       <h2>Our contacts:</h2>
       <p className="contacts-paragraph" onClick={handlePhoneClick}>
       <strong className="contact-label">Tell:</strong><i className="fa fa fa-phone" /> +996 502 015 209
@@ -23,6 +24,7 @@ export default function Contacts() {
         <p className="contacts-paragraph">
           <strong className="contact-label">Working Hours:</strong> Monday to Friday, 9:00 AM - 6:00 PM
         </p>
+        <p className="contacts-paragraph-two">Feel free to contact us at any time. We strive to provide you with the best service and answer all your questions.</p>
       </div>
     </div>
   );
