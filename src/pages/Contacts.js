43b34448837878b69d8ddc7436a704f0750b86df
arsenlenovo <arsenarsarsenars85@gmail.com>
2023-05-24 22:35:15 +0600

    gave the class a name on the paragraphs

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -11,16 +11,16 @@ export default function Contacts() {
       <div className="contact-details">
       <h1>Contacts</h1>
       <h2>Our contacts:</h2>
-      <p onClick={handlePhoneClick}>
+      <p className="contacts-paragraph" onClick={handlePhoneClick}>
       <strong className="contact-label">Tell:</strong><i className="fa fa fa-phone" /> +996 502 015 209
       </p>
-        <p>
+        <p className="contacts-paragraph">
           <strong className="contact-label">Address:</strong> 123 Music Street, Cityville, State, Country
         </p>
-        <p>
+        <p className="contacts-paragraph">
           <strong className="contact-label">Email:</strong> kemelbekovdaniyar0@gmail.com
         </p>
-        <p>
+        <p className="contacts-paragraph">
           <strong className="contact-label">Working Hours:</strong> Monday to Friday, 9:00 AM - 6:00 PM
         </p>
       </div>
