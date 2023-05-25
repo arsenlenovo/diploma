74ddbe699fac817edfd1ff84dc8f2582e87f4f0b
arsenlenovo <arsenarsarsenars85@gmail.com>
2023-05-25 14:02:45 +0600

    add paragraph div

--- src/pages/Delivery.js
+++ src/pages/Delivery.js
@@ -1,8 +1,16 @@
+import "./Delivery.css";
 export default function Delivery() {
   return (
     <div className="Delivery">
       <h1>Delivery</h1>
       <p>This is </p>
+      <div className="main-div">
+         <div className="div-delivery">
+            <p className="delivery-paragraph">We are pleased to offter you a convenient delivery of watches directly to your door. Enjoy a unique selection and reliable service when ordering from us.
+            Want to buy a new watch but don't want to spend time shopping? We offter a convenient shipping service so you can get your favorite watch hassle-free. Place an order on our website and enjoy dropshipping watches at your convenience. Our teame will do everything possible to yu as quickly as possible</p>
+            <p className="delivery-paragraph">We appreciate your time and comfort. Therefore, we offter free delivery of watches within the city. Let us take care of the delivery    while you enjoy choosing from our wide range. Our watch delivery servise quarantees  fast and reliable shipment. No matter where you are, we will delivery your watch safe and sound. Order a watch from us and enjoy express delivery. You won't have to wait long to get your new watch. We strive to make the buying process as convenient as possible for you.</p>
+          </div>
+      </div>
     </div>
   )
 }
