2e3ece3c96e974ecc6e1f2f183013cac794ce12c
arsenlenovo <arsenarsarsenars85@gmail.com>
2023-05-25 19:44:53 +0600

    styled contacts and add network for caontacts

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -24,6 +24,20 @@ export default function Contacts() {
         </p>
         <p className="contacts-paragraph-two">Feel free to contact us at any time. We strive to provide you with the best service and answer all your questions.</p>
       </div>
+      <div className="footer-icon">
+         <h1>Network</h1>
+         <div className="footer-aps">
+         <a href="https://wa.me/qr/AVDLLXHGVDAPJ1" target="blank">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/767px-WhatsApp.svg.png" alt="WhatsApp"/>
+        </a>
+        <a href="https://www.instagram.com/arsensio2006/">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/640px-Instagram_icon.png" alt="linkedln"/> 
+        </a>
+        <a href="https://web.telegram.org/a/">
+          <img src="https://www.svgrepo.com/download/299513/telegram.svg" alt="Telegram"/>
+        </a>
+      </div>
+    </div> 
     </div>
   );
 }
