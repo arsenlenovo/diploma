c2aeeec01859c74486e80d53cf6d6ed6033ca68e
arsenlenovo <arsenarsarsenars85@gmail.com>
2023-05-25 15:31:13 +0600

    add let menu for Navlast Drawer

--- src/components/Nav/NavLast/NavLast.js
+++ src/components/Nav/NavLast/NavLast.js
@@ -3,6 +3,13 @@ import Logo from "../../Logo/Logo";
 import "./NavLast.css";
 
 export default function NavLast() {
+  let menuBtn = document.querySelectorAll('.NavItem a')
+  let menu = document.querySelector('.Drawer')
+    menuBtn.forEach(el => {
+    el.addEventListener('click', function(){
+        menu.classList.remove('open')
+    })
+})
   return (
     <nav className="NavLast">
       <ul className="Ul">
