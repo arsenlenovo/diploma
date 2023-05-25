c8c4306baf0c03563bb8a42845d96d70c54553d5
arsenlenovo <arsenarsarsenars85@gmail.com>
2023-05-25 13:59:57 +0600

    add span new address email

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -9,20 +9,18 @@ export default function Contacts() {
   return (
     <div className="Contacts">
       <div className="contact-details">
-      <h1>Contacts</h1>
-      <p className="contacts-paragraph-two">If you have questions, suggestions or want to leave a review, we are always happy to help. We are always ready to listen to your questions, accept your suggestions and take into account your feedback. Contact us right now and we will be happy to help you! Please choose your preferred contact method from the options below:</p>
-      <h2>Our contacts:</h2>
-      <p className="contacts-paragraph" onClick={handlePhoneClick}>
-      <strong className="contact-label">Tell:</strong><i className="fa fa fa-phone" /> +996 502 015 209
-      </p>
-        <p className="contacts-paragraph">
-          <strong className="contact-label">Address:</strong> 123 Music Street, Cityville, State, Country
+         <h1>Contacts</h1>
+         <p className="contacts-paragraph-two">If you have questions, suggestions or want to leave a review, we are always happy to help. We are always ready to listen to your questions, accept your suggestions and take into account your feedback. Contact us right now and we will be happy to help you! Please choose your preferred contact method from the options below:</p>
+        <h2>Our contacts:</h2>
+        <p className="contacts-paragraph" onClick={handlePhoneClick}>
+        <strong className="contact-label">Tell:</strong>
+        <i className="fa fa fa-phone" /><span className="fg">+996 502 015 209</span>
         </p>
         <p className="contacts-paragraph">
-          <strong className="contact-label">Email:</strong> kemelbekovdaniyar0@gmail.com
+          <strong className="contact-label">Address:</strong> VJ4M+Q9J, Bishkek region, Country Kyrgyzstan
         </p>
         <p className="contacts-paragraph">
-          <strong className="contact-label">Working Hours:</strong> Monday to Friday, 9:00 AM - 6:00 PM
+          <strong className="contact-label">Email:</strong> arsenarsensio2006@gmail.com
         </p>
         <p className="contacts-paragraph-two">Feel free to contact us at any time. We strive to provide you with the best service and answer all your questions.</p>
       </div>
